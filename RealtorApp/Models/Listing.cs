be89cc7a656cf9733ab37fbbaeec98193fd4fa49
Dante Gutierrez <danteg177@gmail.com>
2023-09-13 15:39:57 -0700

    Created a UI to match design

---
+++ RealtorApp/Models/Listing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtorApp.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public Address Address { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public Details Details { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+    public class Address
+    {
+        public string LineOne { get; set; }
+        public string LineTwo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string AreaCode { get; set; }
+    }
+
+    public class Details
+    {
+        public int Bed { get; set; }
+        public double Bath { get; set; }
+        public int Sqft { get; set; }
+    }
+}
